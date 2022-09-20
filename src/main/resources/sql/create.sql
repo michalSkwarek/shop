99e1cb1df68598bf12d6432469d2de0a93dfbdb7
Michal <skwarek.michal.adrian@gmail.com>
2022-09-20 23:30:27 +0200

    Added simple product entity

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,1 +1,4 @@
 -- ----------------------------------------------
+-- add few products
+INSERT INTO products (id, name) VALUES (1, 'example');
+INSERT INTO products (id, name) VALUES (2, 'example2');
