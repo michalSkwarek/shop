658f168f84ba31030f9cb9a8b72171f7284c4fc5
Michal <skwarek.michal.adrian@gmail.com>
2022-09-20 23:19:30 +0200

    Initialized database connection

--- src/main/java/com/skwarek/shop/ShopApplication.java
+++ src/main/java/com/skwarek/shop/ShopApplication.java
@@ -2,12 +2,20 @@ package com.skwarek.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
+    }
+
+    @GetMapping(value = {"/", "/hello"})
+    public String sayHello() {
+        return "Hello you";
     }
 
 }
