7135c47a1109a95eb1ca391fac3218cd72246ec3
Michal <skwarek.michal.adrian@gmail.com>
2022-09-21 01:10:32 +0200

    Added address entities

---
+++ src/main/java/com/skwarek/shop/model/address/Address.java
@@ -0,0 +1,40 @@
+package com.skwarek.shop.model.address;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street_name")
+    private String streetName;
+
+    @Column(name = "street_number")
+    private String streetNumber;
+
+    @Column(name = "door_number")
+    private String doorNumber;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private Country country;
+
+}
